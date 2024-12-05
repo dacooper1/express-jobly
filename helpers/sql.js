062536503fa99fe654da96c6c3d3984878baddb5
Dejah Cooper <Dejah.C@icloud.com>
2024-12-05 14:55:24 -0500

    added doc strings

--- helpers/sql.js
+++ helpers/sql.js
@@ -2,11 +2,16 @@ const { BadRequestError } = require("../expressError");
 
 // THIS NEEDS SOME GREAT DOCUMENTATION.
 
+/** converts json to sql 
+ * - jsToSql is an object containing dataToUpdate keys in snake_case 
+ */
 function sqlForPartialUpdate(dataToUpdate, jsToSql) {
   const keys = Object.keys(dataToUpdate);
+  // checks for any data to update 
   if (keys.length === 0) throw new BadRequestError("No data");
 
   // {firstName: 'Aliya', age: 32} => ['"first_name"=$1', '"age"=$2']
+  // if the SQL version does not exist, it use the origional key
   const cols = keys.map((colName, idx) =>
       `"${jsToSql[colName] || colName}"=$${idx + 1}`,
   );
