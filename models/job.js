fdd58fb2a14f92b59a6becb9a8f44ac71445f27e
Dejah Cooper <Dejah.C@icloud.com>
2024-12-06 14:27:33 -0500

    added Job.get(id) method

--- models/job.js
+++ models/job.js
@@ -30,7 +30,7 @@ class Job {
           `INSERT INTO jobs
            (company_handle, title, salary, equity)
            VALUES ($1, $2, $3, $4)
-           RETURNING company_handle AS "companyHandle", title, salary, equity`,
+           RETURNING id, company_handle AS "companyHandle", title, salary, equity`,
         [
           companyHandle,
           title,
@@ -50,7 +50,7 @@ class Job {
 
   static async findAll() {
     const jobsRes = await db.query(
-          `SELECT company_handle AS "companyHandle", title, salary, equity
+          `SELECT id, company_handle AS "companyHandle", title, salary, equity
            FROM jobs
            ORDER BY title`);
     return jobsRes.rows;
@@ -64,22 +64,18 @@ class Job {
    * Throws NotFoundError if not found.
    **/
 
-  static async get(handle) {
-    const companyRes = await db.query(
-          `SELECT handle,
-                  name,
-                  description,
-                  num_employees AS "numEmployees",
-                  logo_url AS "logoUrl"
-           FROM companies
-           WHERE handle = $1`,
-        [handle]);
+  static async get(id) {
+    const jobRes = await db.query(
+          `SELECT id, company_handle AS "companyHandle", title, salary, equity
+            FROM jobs
+           WHERE id = $1`,
+        [id]);
 
-    const company = companyRes.rows[0];
+    const job = jobRes.rows[0];
 
-    if (!company) throw new NotFoundError(`No company: ${handle}`);
+    if (!job) throw new NotFoundError(`No job: ${handle}`);
 
-    return company;
+    return job;
   }
 
   static async filter(queryData) {
