cd86efae20ce2cf3e746e1416569044e9e6844e3
Dejah Cooper <Dejah.C@icloud.com>
2024-12-06 15:47:59 -0500

    changed the company handle

---
+++ routes/jobs.test.js
@@ -0,0 +1,84 @@
+"use strict";
+
+const request = require("supertest");
+
+const db = require("../db");
+const app = require("../app");
+
+const {
+  commonBeforeAll,
+  commonBeforeEach,
+  commonAfterEach,
+  commonAfterAll,
+  u1Token,
+  u4Token
+
+} = require("./_testCommon");
+
+beforeAll(commonBeforeAll);
+beforeEach(commonBeforeEach);
+afterEach(commonAfterEach);
+afterAll(commonAfterAll);
+
+/************************************** POST /jobs */
+
+describe("POST /jobs", () => {
+    test("works for admin", async () => {
+      const resp = await request(app)
+        .post("/jobs")
+        .send({
+          companyHandle: "c1",
+          title: "new-job",
+          salary: 120000,
+          equity: 0.1,
+        })
+        .set("Authorization", `Bearer ${u4Token}`);
+      expect(resp.statusCode).toBe(201);
+      expect(resp.body).toEqual({
+        job: {
+          id: expect.any(Number),
+          companyHandle: "c1",
+          title: "new-job",
+          salary: 120000,
+          equity: "0.1",
+        },
+      });
+    });
+  
+    test("unauth for non-admin", async () => {
+      const resp = await request(app)
+        .post("/jobs")
+        .send({
+          companyHandle: "test-handle",
+          title: "new-job",
+          salary: 120000,
+          equity: 0.1,
+        })
+        .set("Authorization", `Bearer ${u1Token}`);
+      expect(resp.statusCode).toBe(401);
+    });
+  
+    test("bad request with missing data", async () => {
+      const resp = await request(app)
+        .post("/jobs")
+        .send({
+          companyHandle: "test-handle",
+        })
+        .set("Authorization", `Bearer ${u4Token}`);
+      expect(resp.statusCode).toBe(400);
+    });
+  
+    test("bad request with invalid data", async () => {
+      const resp = await request(app)
+        .post("/jobs")
+        .send({
+          companyHandle: "test-handle",
+          title: "new-job",
+          salary: "not-a-number",
+          equity: 0.1,
+        })
+        .set("Authorization", `Bearer ${u4Token}`);
+      expect(resp.statusCode).toBe(400);
+    });
+  });
+  
