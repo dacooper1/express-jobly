631ac048286386e19f6e3068d06d513583aef0b4
Dejah Cooper <Dejah.C@icloud.com>
2024-12-06 14:00:29 -0500

    added POST /jobs route

--- routes/jobs.js
+++ routes/jobs.js
@@ -10,7 +10,7 @@ const { ensureLoggedIn, ensureAdmin} = require("../middleware/auth");
 const Job = require("../models/job");
 
 const jobNewSchema = require("../schemas/jobNew.json");
-const jobUpdateSchema = require("../schemas/jobUpdate.json");
+// const jobUpdateSchema = require("../schemas/jobUpdate.json");
 
 const router = new express.Router();
 
